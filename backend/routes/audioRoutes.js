6492f96499750f9604bc38e0bfdb13d463e3093d
Aaron <erinadost@gmail.com>
2024-03-08 15:57:27 -0500

    update backend

---
+++ backend/routes/audioRoutes.js
@@ -0,0 +1,43 @@
+// routes/videoRoutes.js
+const express = require('express');
+const router = express.Router();
+const AWS = require('aws-sdk');
+const { parseRangeHeader } = require('../utils/helpers');
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const s3 = new AWS.S3();
+
+router.get('/:filename', (req, res) => {
+  const { filename } = req.params;
+  const params = {
+    Bucket: 'space.bucket',
+    Key: filename,
+  };
+
+  const range = req.headers.range;
+  if (!range) {
+    return res.status(400).send('Range header is required');
+  }
+
+  const { start, end } = parseRangeHeader(range);
+  params.Range = `bytes=${start}-${end}`;
+
+  s3.getObject(params, (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send('Internal Server Error');
+    }
+
+    res.setHeader('Content-Type', 'audio/mp3');
+    res.setHeader('Content-Range', `bytes ${start}-${end}/${data.ContentLength}`);
+    res.setHeader('Accept-Ranges', 'bytes');
+    res.send(data.Body);
+  });
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,44 @@
+// server.js
+const express = require('express');
+const cors = require('cors');
+const AWS = require('aws-sdk');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // Enable JSON parsing
+
+// AWS SDK Configuration
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const s3 = new AWS.S3();
+
+// Helper function to parse Range header
+function parseRangeHeader(range) {
+  const [, start, end] = range.match(/(\d+)-(\d+)?/);
+  return {
+    start: parseInt(start, 10),
+    end: end ? parseInt(end, 10) : undefined,
+  };
+}
+
+// Routes
+const videoRoutes = require('./routes/videoRoutes');
+const audioRoutes = require('./routes/audioRoutes');
+const photoRoutes = require('./routes/photoRoutes');
+
+// Use routes
+app.use('/api/videos', videoRoutes);
+app.use('/api/audio', audioRoutes);
+app.use('/api/photos', photoRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
