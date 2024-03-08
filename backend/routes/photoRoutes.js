a4d4b096fb0a2f8c2f168b8df4522d9014f771aa
Aaron <erinadost@gmail.com>
2024-03-08 18:25:59 -0500

    rework routes

--- backend/routes/photoRoutes.js
+++ backend/routes/photoRoutes.js
@@ -1,4 +1,4 @@
-// routes/videoRoutes.js
+// routes/photoRoutes.js
 const express = require('express');
 const router = express.Router();
 const AWS = require('aws-sdk');
@@ -19,14 +19,6 @@ router.get('/:filename', (req, res) => {
     Key: filename,
   };
 
-  const range = req.headers.range;
-  if (!range) {
-    return res.status(400).send('Range header is required');
-  }
-
-  const { start, end } = parseRangeHeader(range);
-  params.Range = `bytes=${start}-${end}`;
-
   s3.getObject(params, (err, data) => {
     if (err) {
       console.error(err);
@@ -34,8 +26,6 @@ router.get('/:filename', (req, res) => {
     }
 
     res.setHeader('Content-Type', 'image/png');
-    res.setHeader('Content-Range', `bytes ${start}-${end}/${data.ContentLength}`);
-    res.setHeader('Accept-Ranges', 'bytes');
     res.send(data.Body);
   });
 });
--- backend/utils/helpers.js
+++ backend/utils/helpers.js
@@ -1,10 +1,16 @@
 // Helper function to parse Range header
 function parseRangeHeader(range) {
-    const [, start, end] = range.match(/(\d+)-(\d+)?/);
-    return {
-      start: parseInt(start, 10),
-      end: end ? parseInt(end, 10) : undefined,
-    };
-  }
-  
-  module.exports = { parseRangeHeader };
+  const [, start, end] = range.match(/(\d+)-(\d+)?/);
+  const parsedStart = parseInt(start, 10);
+  const parsedEnd = end ? parseInt(end, 10) : undefined;
+
+  // Ensure a default value for parsedEnd
+  const defaultEnd = parsedEnd || parsedStart + 999999;
+
+  return {
+    start: parsedStart,
+    end: defaultEnd,
+  };
+}
+
+module.exports = { parseRangeHeader };
